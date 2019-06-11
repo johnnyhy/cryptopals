b42167f3450b0c2f3d8bfc420b2deabd10c77ca4
Johnathan Yang <johnnyhy@umich.edu>
2019-06-10 22:25:04 -0400

    beginning caesar cipher

---
+++ set1/3.caesar_cipher/caesar_cipher.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+string caesar(string plaintext, char key) {
+
+    string ciphertext;
+
+    return ciphertext;
+}
+
+int main() {
+    for (char i = 0; i < 16; i++) {
+        cout << caesar("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736", i) << endl;
+    }
+}
